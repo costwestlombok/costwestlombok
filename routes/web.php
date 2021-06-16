1e1022b6c34745ebf40a9c5dc11a0933d89e69b9
ridwanali <ridwanali404@gmail.com>
2021-06-16 11:38:58 +0800

    udpate

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,7 @@
 |
  */
 
-Route::get('oc4ids', 'FrontController@oc4ids');
+Route::get('oc4ids', ['middleware' => 'cors' , 'uses'=> 'FrontController@oc4ids']);
 Route::get('lang/{language}', 'LocalizationController@switch')->name('localization.switch');
 Route::get('/', 'FrontController@index');
 
